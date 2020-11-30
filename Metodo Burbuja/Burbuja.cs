bec59a891603197ae1ead33a0f0cad3562c04423
Arguijo Vazquez <arguijovazquez@hotmail.com>
2020-11-30 13:25:02 -0600

    Metodo Burbuja

---
+++ Metodo Burbuja/Burbuja.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodo_Burbuja
+{
+    class Burbuja
+    {
+        //variable privada para las pocision
+        private int Pos;
+        //Variable auxiliar privada
+        private int aux;
+        //Constructor
+        public Burbuja()
+        {
+            Pos = 0;
+        }
+        //Metod para Insertar elementos
+        public int Insertar(int[] valores, int elemento)
+        {
+            valores[Pos] = elemento;
+            Pos++;
+            return (Pos);
+        }
+        // Metodo para Intercambiar posiciones(Metodo Burbuja)
+        public void Intercambio(int[] valores)
+        {
+            for(int i= 0;i < Pos; i++)
+            for(int j = Pos - 1;j > i; j--)
+            {
+            	if(valores[j-1] > valores[j])
+            	{
+            		aux=valores[j-1];
+            		valores[j-1] = valores[j];
+            		valores[j] = aux;
+            	}
+            }
+        }
+               //Metodo para Mostrar
+                public void Mostrar(int[] valores)
+                {
+                    //arreglo para mostrar valores
+                    for (int i = 0; i <=7; i++)
+                    {
+                        //Muestra los valores del arreglo
+                        Console.WriteLine("Elemento[ " + i + " ]" + " = " +valores[i]);
+                    }
+                }
+        
+          }
+      }
---
+++ Metodo Burbuja/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodo_Burbuja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------------------------------Metodo Burbuja------------------------------------");
+            Console.WriteLine("");
+            //Arreglo con un tamaño de 10 datos
+            int[] Arreglo = new int [10];
+            //Declaracion de objeto
+            Burbuja Obj1 = new Burbuja();
+
+            Obj1.Insertar(Arreglo,4);
+            Obj1.Insertar(Arreglo,8);
+            Obj1.Insertar(Arreglo,90);
+            Obj1.Insertar(Arreglo,23);
+            Obj1.Insertar(Arreglo,1);
+            Obj1.Insertar(Arreglo,67);
+            Obj1.Insertar(Arreglo,54);
+            Obj1.Insertar(Arreglo,16);
+
+            Console.Write("---------------------------------- Elementos aun no ordenados: --------------------------");
+            Console.WriteLine();
+            Obj1.Mostrar(Arreglo);
+            Obj1.Intercambio(Arreglo);
+            Console.Write("----------------------------- Elementos Ordenados de Menor a Mayor: ----------------------");
+            Console.WriteLine();
+            Obj1.Mostrar(Arreglo);
+            Console.ReadKey();
+        }
+
+        }
+    }
